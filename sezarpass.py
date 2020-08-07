fbed2edc4170238c5bc9e136b67cb603d8fda23a
Kupass01 <57138087+Kupass01@users.noreply.github.com>
2020-08-08 01:10:52 +0300

    Add files via upload

---
+++ sezarpass.py
@@ -0,0 +1,46 @@
+#sezar şifreleme
+dongu = 1
+menu ="""
+Python sezar şifreleme
+örnek : özjğpglu 
+////////////////
+[1] Şifrele
+[2] Şifre çöz
+[3] Çıkış
+"""
+while dongu == 1:
+    print(menu)
+    sorgu = input(">>")
+    if(sorgu == ""):
+        print("boş bırakmayınız !")
+    elif(sorgu == "1"):
+        kelimen = []
+        harfler = ["a","b","c","ç","d","e","f","g","ğ","h","ı","i","j","k","l","m","n","o","ö","p","r","s","ş","t","u","ü","v","y","z","q","w"," x"]
+        kelime = input("kelime:")
+        kelime.replace(" ","")
+        for harf in kelime:
+            if(harf in harfler):  
+                sıra = harfler.index(harf)
+                yeni = harfler[(sıra+3)%len(harfler)]
+                kelimen.append(yeni)
+                print(*kelimen)
+            else:
+                print("böyle bir harf yok!")
+    elif(sorgu == "2"):
+        kelimen = []
+        harfler = ["a","b","c","ç","d","e","f","g","ğ","h","ı","i","j","k","l","m","n","o","ö","p","r","s","ş","t","u","ü","v","y","z","q","w"," x"]
+        kelime = input("kelime:")
+        kelime.replace (" ","")
+        for harf in kelime:
+            if(harf in harfler):  
+                sıra = harfler.index(harf)
+                yeni = harfler[(sıra-3)%len(harfler)]
+                kelimen.append(yeni)
+                print(*kelimen)
+            else:
+                print("böyle bir harf yok!")
+    elif(sorgu == "3"):
+        print("program kapatıldı!")
+        dongu = 0
+    else:
+        print("böyle bir komut yok!")
